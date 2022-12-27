0cd5ff45610533250a7ecc46d7aa3f9f192b1062
Tom Virgo <t.virgo@iwoca.co.uk>
2022-12-27 12:02:11 +0000

    add lineup and odss endpoints

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Message": "Welcome to your six nations fantasy bot.",
+        "Endpoints": {
+            "/bot/lineups/{season_id}": "get lineups for that season id",
+            "/bot/probabilities/{season_id}": "get odds of winning team for that season id",
+        },
+    }
+
+
+@app.get("/bot/lineups/{season_id}")
+def read_item(season_id: str):
+    return {"season_id": season_id}
+
+
+@app.get("/bot/probabilities/{season_id}")
+def read_item(season_id: str):
+    return {"season_id": season_id}
